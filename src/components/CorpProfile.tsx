0102e4ae933a53bd9759ef694f97424acf5ba5f3
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-07 13:00:05 +0900

    회사 프로필 카드 수정

--- src/components/CorpProfile.tsx
+++ src/components/CorpProfile.tsx
@@ -1,67 +1,78 @@
 import React from 'react';
-import { Theme, Avatar, Box, Card, Flex, Text } from '@radix-ui/themes';
-
+import { Theme, Avatar, Box, Card, Inset, Flex, Text, Link } from '@radix-ui/themes';
+import { ExternalLinkIcon } from '@radix-ui/react-icons';
 export interface CorpProfileProps {
   name: string;
   nationality: string;
-  ceo_name: string;
   corp_address: string;
   corp_num: string;
-  biz_started_at: Date;
+  review_count: number;
   biz_type: string;
   logo_image: string;
+  homepage_url: string;
 }
 
 const CorpProfile: React.FC<CorpProfileProps> = ({
   name,
   nationality,
-  ceo_name,
   corp_address,
-  corp_num,
-  biz_started_at,
+  review_count,
   biz_type,
   logo_image,
+  homepage_url,
 }) => {
   return (
     <Theme>
-      <Box style={{ width: '800px', padding: '24px', margin: '16px 0' }}>
-        <Card>
-          <Flex direction="column" gap="4" align="start">
-            <Flex direction="row" align="center" gap="4">
-              <Avatar src={logo_image} size="7" alt={`${name} logo`} radius="full" fallback={name.charAt(0)}/>
-              <Box>
-                <Text as="div" size="5" weight="bold">
-                  {name}
-                </Text>
-                <Text as="p" color="gray" size="2">
-                  {biz_type}
-                </Text>
-                <Text as="p" color="gray" size="2">
-                  {nationality}
-                </Text>
-              </Box>
-            </Flex>
-            <Flex direction="row" gap="6" style={{ marginTop: '16px' }}>
-              <Box>
-                <Text as="p" size="3" weight="bold">CEO</Text>
-                <Text as="p" size="3">{ceo_name}</Text>
-              </Box>
-              <Box>
-                <Text as="p" size="3" weight="bold">Address</Text>
-                <Text as="p" size="3">{corp_address}</Text>
-              </Box>
-              <Box>
-                <Text as="p" size="3" weight="bold">Corp Number</Text>
-                <Text as="p" size="3">{corp_num}</Text>
-              </Box>
-              <Box>
-                <Text as="p" size="3" weight="bold">Biz Started At</Text>
-                <Text as="p" size="3">{biz_started_at.toLocaleDateString()}</Text>
-              </Box>
-            </Flex>
-          </Flex>
-        </Card>
-      </Box>
+      <Flex>
+        <Box style={{ width: '800px', padding: '24px', margin: '16px 0' }}>
+          <Card>
+            <Inset>
+              <img
+                  src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                  alt="Bold typography"
+                  style={{
+                    display: "block",
+                    objectFit: "cover",
+                    width: "100%",
+                    height: 140,
+                    backgroundColor: "var(--gray-5)",
+                  }}
+               />
+            </Inset>
+              <Flex direction="column" gap="4" align="start">
+                <Flex direction="row" align="center" gap="4">
+                  <Avatar src={logo_image} size="7" alt={`${name} logo`} radius="full" fallback={name.charAt(0)}/>
+                  <Box>
+                    <Text as="div" size="5" weight="bold">
+                      {name}
+                    </Text>
+                    <Text as="p" color="gray" size="2">
+                      {biz_type}
+                    </Text>
+                    <Text as="p" color="gray" size="2">
+                      {nationality}
+                    </Text>
+                  </Box>
+                </Flex>
+                <Flex direction="row" gap="1" style={{ marginTop: '16px' }}>
+                  <Box>
+                    <Text as="p" size="3" weight="bold" color="gray">{corp_address}</Text>
+                  </Box>
+                  {'\u00b7'}
+                  <Box>
+                    <Text as="p" size="3" weight="bold" color="gray">리뷰 {review_count}건</Text>
+                  </Box>
+                  <Box pl="3">
+                    <Link href={homepage_url} weight="bold">
+                      홈페이지
+                      <ExternalLinkIcon/>
+                    </Link>
+                  </Box>
+                </Flex>
+              </Flex>
+          </Card>
+        </Box>
+      </Flex>
     </Theme>
   );
 };
