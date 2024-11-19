9de03a0c1b40d3dac94a82adb8448ce371eb4b5c
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-19 10:44:38 +0900

    빌드 관련 설정 추가
    rollup 패키지 추가 및 빌드 스크립트 추가
    package.json 파일 수정 - main , module (패키지 진입정) 추가

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+
+export { default as StudentCard } from "./StudentCard";
+export { default as AcademicHistoryCard } from "./AcademicHistoryCard";
+export { default as CorpProfile } from "./CorpProfile";
+export { default as LanguageCard } from "./LanguageCard";
+export { default as RequestCard } from "./RequestCard";
+export { default as RequestProfile } from "./RequestProfile";
+export { default as ReviewOfCorp } from "./ReviewOfCorp";
+export { default as ReviewOfStudent } from "./ReviewOfStudent";
+export { default as ReviewPopOver } from "./ReviewPopOver";
+
