c7e7a8554ef7ed3c7b7259284c3457f5acb33e0f
Min Jeong <jungm8453@gmail.com>
2024-12-11 12:38:13 +0900

    stickybutton onclick 동작 추가

--- src/components/StickyButton.tsx
+++ src/components/StickyButton.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React from "react";
 import { Theme, Button, Flex, Container } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-
 
 export interface StickyButtonProps {
     viewerType: number;
     innerText: string;
+    onClick?: () => void;
 }
 
 const renderButton = (props: StickyButtonProps) => {
     switch (props.viewerType) {
         case 1:
-            return(<Button radius="large" variant="soft" size="4" color="orange"><MagnifyingGlassIcon/>{props.innerText}</Button>)    
+            return (
+                <Button
+                    radius="large"
+                    variant="soft"
+                    size="4"
+                    color="orange"
+                    onClick={props.onClick}
+                >
+                    <MagnifyingGlassIcon />
+                    {props.innerText}
+                </Button>
+            );
         case 2:
-            return(<Button radius="large" variant="soft" size="4" color="blue"><MagnifyingGlassIcon/> {props.innerText}</Button>)    
+            return (
+                <Button
+                    radius="large"
+                    variant="soft"
+                    size="4"
+                    color="blue"
+                    onClick={props.onClick}
+                >
+                    <MagnifyingGlassIcon />
+                    {props.innerText}
+                </Button>
+            );
         default:
-            return (<></>)
+            return <></>;
     }
-}
+};
 
-const StickyButton = (props: StickyButtonProps) => (
+const StickyButton: React.FC<StickyButtonProps> = (props) => (
     <Container position="sticky" bottom="10px" minWidth="300px" maxWidth="500px">
         <Flex direction="column" align="stretch" justify="center" pl="3" pr="3">
             {renderButton(props)}
         </Flex>
     </Container>
-    );
+);
 
-export default StickyButton
+export default StickyButton;
